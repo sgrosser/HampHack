6db1a2c3be53ae73378491abb43c465db69c6bd4
sgrosser <stefan.grosser1@gmail.com>
2016-02-26 19:22:32 -0500

    Basic foundation work for Barrel

---
+++ Barrel/src/Item.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class Item {
+	public String name;
+	public int OriginalStock = 0;
+	public int CurrentStock = 0;
+	public int datePurchased = 0;
+	public int dateRanOut;
+	public int lastingFactor; //attempt to decrease with Go Green
+	public boolean low = false;
+	public int importance;
+	public int quickLoss; // period length of loss of more than 33%
+	public int daysSincePurchase = 0;
+
+	public Item(String name, int origStock, int purchaseDate, int estimatedLast, int imp){
+		this.name=name;
+		this.OriginalStock=origStock;
+		this.CurrentStock = origStock;
+		this.lastingFactor = estimatedLast;
+		this.importance = imp;
+		
+	}
+	
+}
